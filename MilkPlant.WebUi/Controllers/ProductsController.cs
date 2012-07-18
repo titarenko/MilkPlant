94f268f8ae31223c6b2afe10c9982e34780bd048
Constantin Titarenko <constantin.titarenko@gmail.com>
2012-07-18 21:31:08 +0300

    Added best sellers view.

---
+++ MilkPlant.WebUi/Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using MilkPlant.Interfaces;
+
+namespace MilkPlant.WebUi.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IProductAnalytics analytics;
+
+        public ProductsController(IProductAnalytics analytics)
+        {
+            this.analytics = analytics;
+        }
+
+        public ActionResult BestSellers()
+        {
+            return View(analytics.GetBestSellers(10));
+        }
+    }
+}
