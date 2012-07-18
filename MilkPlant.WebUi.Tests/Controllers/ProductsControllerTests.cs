8b2255a1df7988160fdbc2ab0ccb673ec415b196
Constantin Titarenko <constantin.titarenko@gmail.com>
2012-07-18 21:52:27 +0300

    Added tests for web UI.

---
+++ MilkPlant.WebUi.Tests/Controllers/ProductsControllerTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using FizzWare.NBuilder;
+using MilkPlant.Interfaces;
+using MilkPlant.Interfaces.Models;
+using MilkPlant.WebUi.Controllers;
+using NSubstitute;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace MilkPlant.WebUi.Tests.Controllers
+{
+    [TestFixture]
+    public class ProductsControllerTests
+    {
+        [Test]
+        public void BestSellers_Should_Render10BestSellersUsingProductAnalytics()
+        {
+            // arrange
+            var analytics = Substitute.For<IProductAnalytics>();
+            analytics.GetBestSellers(Arg.Any<int>()).Returns(
+                callInfo => Builder<BestSeller>
+                                .CreateListOfSize(callInfo.Arg<int>())
+                                .All()
+                                .With(x => x.Name = "Best")
+                                .Build());
+            var controller = new ProductsController(analytics);
+
+            // act
+            var result = (ViewResult) controller.BestSellers();
+            var model = (IEnumerable<BestSeller>) result.Model;
+
+            // assert
+            model.Should().HaveCount(10);
+            model.Should().OnlyContain(x => x.Name == "Best");
+        }
+    }
+}
