a64a79cd760dd4c30d7e90d54439ce9d41144293
Constantin Titarenko <constantin.titarenko@gmail.com>
2012-07-18 22:12:28 +0300

    Changed default page.

--- MilkPlant.WebUi/Global.asax.cs
+++ MilkPlant.WebUi/Global.asax.cs
@@ -21,7 +21,7 @@ namespace MilkPlant.WebUi
             routes.MapRoute(
                 "Default", // Route name
                 "{controller}/{action}/{id}", // URL with parameters
-                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+                new { controller = "Products", action = "BestSellers", id = UrlParameter.Optional } // Parameter defaults
             );
 
         }
