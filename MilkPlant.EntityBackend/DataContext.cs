5a3d331674e71d0af4e495a5f1116e0533d9b1b8
Constantin Titarenko <constantin.titarenko@gmail.com>
2012-07-18 22:17:37 +0300

    Created Entity Framework powered backend.

---
+++ MilkPlant.EntityBackend/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity;
+using MilkPlant.Interfaces.Models;
+
+namespace MilkPlant.EntityBackend
+{
+    public class DataContext : DbContext
+    {
+        public IDbSet<Product> Products { get; set; }
+
+        public IDbSet<Distributor> Distributors { get; set; }
+
+        public IDbSet<SoldItems> SoldItems { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            Database.SetInitializer(new CreateDatabaseIfNotExists<DataContext>());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
